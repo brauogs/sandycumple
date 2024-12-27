139e0bdcc62f723019210bc4ce19f19095c0edc0
Braulio Omar Guerrero Sánchez <al2193002918@azc.uam.mx>
2024-12-26 22:23:50 -0600

    cambi9o

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function CumpleanosPage() {
         
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-16">
           <LazyImage
-            src="../../sandy.jpg?height=400&width=300"
+            src="../../brau.jpg?height=400&width=300"
             alt="Foto 1"
             width={300}
             height={400}
